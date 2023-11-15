4496db5eb38db2162e52c7c7bfec7247dc205249
RomanL91 <dev_roman91@mail.ru>
2023-11-15 17:11:20 +0600

    функция для переименования загружаемых фотографий

--- core/app_photo_staff/models.py
+++ core/app_photo_staff/models.py
@@ -1,12 +1,21 @@
+from os import path
+
 from django.db import models
 
 from app_staffs.models import Staff
 
 
+def get_path_file(instance, filename):
+    # для переименования загружаемых фото
+    filename = f'{instance.staff.pk}.jpeg'
+    return path.join('staff_photo', filename)
+
+
 class StaffPhoto(models.Model):
-    photo = models.ImageField(verbose_name='Фотография', blank=True, upload_to='staff_photo/')
+    photo = models.ImageField(verbose_name='Фотография', blank=True, upload_to=get_path_file)
     desc = models.TextField(verbose_name='Описание', max_length=1500, blank=True)
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE, verbose_name='Сотрудник')
+
 
     class Meta:
             verbose_name = 'Фотография'
