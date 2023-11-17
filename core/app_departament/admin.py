267ed5a3596727f16d68667698645a226fa81064
RomanL91 <dev_roman91@mail.ru>
2023-11-17 15:32:17 +0600

    админка Департамент

--- core/app_departament/admin.py
+++ core/app_departament/admin.py
@@ -5,4 +5,5 @@ from app_departament.models import Departament
 
 @admin.register(Departament)
 class DepartamentAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['name_departament']
+    list_display = ['name_departament', 'interception', 'desc_dep']
