4c351fd0620169e8db97b275c158d67fe58b0c7c
RomanL91 <dev_roman91@mail.ru>
2023-11-17 15:24:16 +0600

    модель департамента

--- core/app_departament/models.py
+++ core/app_departament/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Departament(models.Model):
+    name_departament = models.CharField(
+        verbose_name='Департамент', unique=True, max_length=200
+    )
+    desc_dep = models.TextField(
+        verbose_name='Описание', blank=True
+    )
+    interception = models.BooleanField(
+        verbose_name='Перехват', default=False,
+        help_text='''Ограничит доступ на всех проходных/контроллерах сотрудников<br>
+        принадлежащие к данному Департаменту'''
+    )
+
+
+    class Meta:
+        verbose_name = 'Департамент'
+        verbose_name_plural = 'Департаменты'
+
+
+    def __str__(self) -> str:
+        return self.name_departament
+    
