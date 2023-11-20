5f1fd5b64bb5ff8752941058bfccbe588a643364
RomanL91 <dev_roman91@mail.ru>
2023-11-20 14:15:45 +0600

    моделька Проходные

--- core/app_checkpoint/apps.py
+++ core/app_checkpoint/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class AppCheckpointConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_checkpoint'
+    verbose_name = 'Управление проходными'
+    
