2aaf6d4af637645de8a141cd27c4c136882f8c27
RomanL91 <dev_roman91@mail.ru>
2023-11-20 14:17:21 +0600

    регистрация в админке без настройки отображения

--- core/app_checkpoint/admin.py
+++ core/app_checkpoint/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_checkpoint.models import Checkpoint
+
+
+@admin.register(Checkpoint)
+class CheckpointAdmin(admin.ModelAdmin):
+    pass
