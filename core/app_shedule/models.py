401bebfc75b20c0be9ec5c2608e720b81a980667
RomanL91 <dev_roman91@mail.ru>
2023-11-21 11:31:35 +0600

    базовая модель для Расписания

--- core/app_shedule/models.py
+++ core/app_shedule/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+from datetime import datetime, time, date, timedelta
+
+
+ver_name_start_d = 'начало рабочего дня'
+ver_name_end_d = 'конец рабочего дня'
+
+def get_time_choices(start_time=time(0,0,0), end_time=time(23,0,0), delta=timedelta(minutes=5)):
+    time_choices = ()
+    time = start_time
+    while time <= end_time:
+        time_choices += ((time, time.strftime("%H:%M")),)
+        time = (datetime.combine(date.today(), time) + delta).time()
+    return time_choices
+
+
+class Schedule(models.Model):
+    name_schedule = models.CharField(
+        verbose_name='Название расписания', max_length=200, unique=True,
+    )
+    desc_schedule = models.TextField(
+        verbose_name='Описание расписания', blank=True
+    )
+    monday_time_stert =  models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True,)
+    monday_time_end =  models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True,)
+    
+    tuesday_time_start =  models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True,)
+    tuesday_time_end =  models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True,)
+    
+    wednesday_time_start = models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True,)
+    wednesday_time_end = models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True,)
+    
+    thursday_time_start = models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True,)
+    thursday_time_end = models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True,)
+    
+    friday_time_start = models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True,)
+    friday_time_end = models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True,)
+    
+    saturday_time_start = models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True,)
+    saturday_time_end = models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True,)
+    
+    sunday_time_start = models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True,)
+    sunday_time_end = models.TimeField(
+        choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True,)
+
+
+    class Meta:
+        verbose_name = 'Расписание'
+        verbose_name_plural = 'Расписания'
+
+
+    def __str__(self) -> str:
+        return self.name_schedule
