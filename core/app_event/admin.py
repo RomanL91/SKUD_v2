487353cb0766769ad886d90be6a90cec461e4950
RomanL91 <dev_roman91@mail.ru>
2023-11-24 16:17:35 +0600

    правки в форме контроллеров и админке событий

--- core/app_event/admin.py
+++ core/app_event/admin.py
@@ -5,4 +5,10 @@ from app_event.models import Event
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'event_staff',
+        'event_date_time',
+        'event_checkpoint',
+        'event_direction',
+        'event_granted',
+    ]
