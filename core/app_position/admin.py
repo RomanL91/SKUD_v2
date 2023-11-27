c280304a6013d80cbefbfef94a61b45c27feb307
RomanL91 <dev_roman91@mail.ru>
2023-11-27 11:59:49 +0600

    лист дисплай в Позиции

--- core/app_position/admin.py
+++ core/app_position/admin.py
@@ -5,5 +5,8 @@ from app_position.models import Position
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
+    list_display = [
+        'name_position', 'desc_position',
+    ]
     search_fields = ['name_position',]
     filter_horizontal = ['tag',]
