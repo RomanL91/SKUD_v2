9fe9a6383e3766ea9b447551acfee2ef1ee4fcd7
RomanL91 <dev_roman91@mail.ru>
2023-11-27 11:15:18 +0600

    перехват по Тегу в сотрудниках

--- core/app_tag/admin.py
+++ core/app_tag/admin.py
@@ -4,6 +4,7 @@ from django import forms
 from django.utils.html import format_html
 
 from app_tag.models import Tags, ColorField
+from app_staffs.models import Staff
 
 
 @admin.register(Tags)
@@ -23,3 +24,13 @@ class TagAdmin(admin.ModelAdmin):
             obj.color_tad,
         )
     get_color_tag.short_description = 'Цвет'
+
+
+    def save_model(self, request, obj, form, change):
+        objs_to_update = []
+        all_staffs_this_dep = obj.staff_set.all()
+        for staff in all_staffs_this_dep:
+            staff.interception = obj.interception
+            objs_to_update.append(staff)
+        Staff.objects.bulk_update(objs_to_update, ['interception'])
+        obj.save()
