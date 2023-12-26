4eb83429214ccc015f023a725a1429d815ca4f83
RomanL91 <dev_roman91@mail.ru>
2023-12-26 11:18:58 +0600

    изменения пркдставления для получения сигнала

--- core/core/base_views.py
+++ core/core/base_views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import redirect
 
-from django.http import JsonResponse
-
 from django.views.decorators.csrf import csrf_exempt
 
-from core.utils import BaseAdapterForModels
-
-from django.utils import timezone
+from core.BaseAdapter import BaseAdapterForModels
 
 
 @csrf_exempt
@@ -17,14 +13,5 @@ def controller_request_receiver_gateway(request):
 
     adapter = BaseAdapterForModels(request_adaptee=request)    
 
-    try:
-        req_to_json, operition_type = adapter.to_json()
-    except TypeError as e:
-        resp = JsonResponse(data={'TypeError': str(e)})
-        resp.status_code = 400
-        return resp
-    
-    if req_to_json is not None:
-        print(f'======== Тип операции {operition_type} ======== {timezone.now()}')
-        adapter.operition_type = operition_type
-        return adapter.adapt_and_save()
+    adapter.get_input_data()
+    return adapter.adapt_and_save_2()
